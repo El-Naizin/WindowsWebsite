e92c96bf9fd4a7d8b1090753f6f06b293011cb81
Yaathavan <yaathavan.kumarasamy@etu.utc.fr>
2023-05-20 14:43:03 +0200

    Ajout Composant Window

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import FooterItem from './components/partials/footer.vue'
+import Window from './components/partials/window.vue'
 </script>
 
 <template>
@@ -21,41 +20,11 @@ import FooterItem from './components/partials/footer.vue'
 
   </main>
 -->
-  <div>
-    <fieldset>
-      <fieldset>
-        <legend>Quoicoubeh</legend>
-        <div class="field-row">
-          <input id="radio17" type="radio" name="fieldset-example2">
-          <label for="radio17">Claire Saffitz</label>
-        </div>
-        <div class="field-row">
-          <input id="radio18" type="radio" name="fieldset-example2">
-          <label for="radio18">Brad Leone</label>
-        </div>
-        <div class="field-row">
-          <input id="radio19" type="radio" name="fieldset-example2">
-          <label for="radio19">Chris Morocco</label>
-        </div>
-        <div class="field-row">
-          <input id="radio20" type="radio" name="fieldset-example2">
-          <label for="radio20">Carla Lalli Music</label>
-        </div>
-      </fieldset>
-      <fieldset>
-        <legend>T'as les cramptés ?</legend>
-        <button>Click me BRO</button>
-      </fieldset>
-    </fieldset>
-  </div>
-
   <div class="window">
-    <div class="title-bar">
-      <div class="title-bar-text">FAT 128 LIFE</div>
-      <div class="title-bar-controls">
-        <button aria-label="Close"></button>
-      </div>
-    </div>
+    <Window title="FAT 128 LIFE" help_btn>
+      <p>Mate moi ça, une fenêtre Windows XP quoi !</p>
+      <p>Quoicoubeh eheh</p>
+    </Window>
   </div>
 
   <div class="footer">
@@ -71,11 +40,6 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 @media (min-width: 1024px) {
   header {
     display: flex;
@@ -83,11 +47,8 @@ header {
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
+  header {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
---
+++ src/components/partials/window.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="window">
+    <div class="title-bar">
+      <div class="title-bar-text">{{title}}</div>
+      <div class="title-bar-controls">
+        <button aria-label="Help" v-if="help_btn"></button>
+        <button aria-label="Minimize" v-if="minimize_btn"></button>
+        <button aria-label="Restore" v-if="restore_btn"></button>
+        <button aria-label="Close" v-if="close_btn"></button>
+      </div>
+    </div>
+    <div class="window-body">
+      <slot>
+        This will only be displayed if there is no content to be distributed.
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "window",
+  props: {
+    title : String,
+    help_btn : Boolean,
+    minimize_btn : Boolean,
+    restore_btn : Boolean,
+    close_btn : Boolean,
+  }
+}
+</script>
+
+<style scoped>
+.title-bar-text {
+  font-family: Tahoma, sans-serif;
+}
+</style>
