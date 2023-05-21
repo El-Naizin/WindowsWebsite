35665aacacf49fa89dab5c69063039db166939c7
Yaathavan <yaathavan.kumarasamy@etu.utc.fr>
2023-05-21 17:53:09 +0200

    Ajout Vérification login/mdp sur Home

--- src/components/partials/content_windows/Home.vue
+++ src/components/partials/content_windows/Home.vue
@@ -6,16 +6,17 @@ import Window from '@/components/partials/Window.vue'
 <Window title="Ouverture de session Windows">
   <div class="field-row">
     <label for="text21">Nom d'utilisateur :</label>
-    <input id="text21" value="Ackow_" type="text" />
+    <input id="text21" v-model="login" type="text" />
   </div>
   <div class="field-row">
     <label for="text21">Mot de passe :</label>
-    <input id="text21" value="P1r@te" type="password" />
+    <input id="text21" v-model="password" type="password" />
   </div>
   <div id="btn_login">
-    <a href="#/">
+    <a href="#/" v-if="login==='Ackow_' && password==='P1r@te'">
       <button class="btn_ok">OK</button>
     </a>
+    <button class="btn_ok" v-else>OK</button>
     <button class="btn_cancel" disabled>Annuler</button>
   </div>
 </Window>
@@ -24,7 +25,13 @@ import Window from '@/components/partials/Window.vue'
 <script>
 export default {
   name: "Home",
-}
+  data() {
+    return {
+      login: 'Ackow_',
+      password: 'P1r@te',
+    };
+  },
+};
 </script>
 
 <style scoped>
