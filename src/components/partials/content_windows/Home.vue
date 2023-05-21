08b3cddf512bd51f9b0b40c1b464a103a72cb550
Leo Naizin <lnaizin@gmail.com>
2023-05-21 18:31:20 +0200

    Fixed Home page routing

--- src/components/partials/content_windows/Home.vue
+++ src/components/partials/content_windows/Home.vue
@@ -2,26 +2,6 @@
 import Window from '@/components/partials/Window.vue'
 </script>
 
-<template>
-<Window title="Ouverture de session Windows">
-  <div class="field-row">
-    <label for="text21">Nom d'utilisateur :</label>
-    <input id="text21" v-model="login" type="text" />
-  </div>
-  <div class="field-row">
-    <label for="text21">Mot de passe :</label>
-    <input id="text21" v-model="password" type="password" />
-  </div>
-  <div id="btn_login">
-    <a href="#/" v-if="login==='Ackow_' && password==='P1r@te'">
-      <button class="btn_ok">OK</button>
-    </a>
-    <button class="btn_ok" v-else>OK</button>
-    <button class="btn_cancel" disabled>Annuler</button>
-  </div>
-</Window>
-</template>
-
 <script>
 export default {
   name: "Home",
@@ -31,8 +11,35 @@ export default {
       password: 'P1r@te',
     };
   },
-};
+  methods: {
+    clickLogin() {
+      if (this.login === 'Ackow_' && this.password === 'P1r@te') {
+        this.$router.push('/lore')
+      }
+    },
+
+  }
+}
 </script>
+
+<template>
+  <Window title="Ouverture de session Windows">
+    <div class="field-row">
+      <label for="text21">Nom d'utilisateur :</label>
+      <input id="text21" v-model="login" type="text"/>
+    </div>
+    <div class="field-row">
+      <label for="text21">Mot de passe :</label>
+      <input id="text21" v-model="password" type="password"/>
+    </div>
+    <div id="btn_login">
+      <button class="btn_ok" @click="clickLogin">Login</button>
+      <!--    <button class="btn_ok" v-else>Login</button>-->
+      <!--    <button class="btn_cancel" disabled>Annuler</button>-->
+    </div>
+  </Window>
+</template>
+
 
 <style scoped>
 #btn_login, .field-row {
